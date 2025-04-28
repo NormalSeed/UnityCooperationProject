2a607af2fcafd089f391092c0356ee137d2c6d60
munsung143 <akunta3475@gmail.com>
2025-04-28 15:55:46 +0900

    [Docs] 주석 추가

--- Assets/KMS/GameManager.cs
+++ Assets/KMS/GameManager.cs
@@ -8,21 +8,27 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
+// 게임 매니저 컴포넌트입니다. 싱글톤을 상속받아 게임 내에 하나만 존재하며, 씬 전환시에도 사라지지 않습니다.
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] private string gameName = "공 굴리기 게임";
+    [SerializeField] private string gameTitle = "공 굴리기 게임";
     [SerializeField] private bool isGameOver;
     [SerializeField] private int score;
     [SerializeField] private int maxScore;
+    // 게임 오버 시에 실행될 이벤트들을 지정합니다.
     [SerializeField] private UnityEvent gameOverEvent;
 
+    // 바로 이전에 열렸던 씬의 인덱스를 저장합니다. 최초 -1
     private int lastOpenedSceneIndex;
+    // 현재 열려있는 씬의 인덱스를 저장힙니다.
     private int currentSceneIndex;
+    public string GameTitle {  get { return gameTitle; } }
     public bool IsGameOver { get { return isGameOver; } }
     public int Score { get { return score; } }
     public int LastOpendSceneIndex { get { return lastOpenedSceneIndex; } }
     public int CurrentSceneIndex {  get { return currentSceneIndex; } }
     
+    // 현재 게임에 빌드되어 있는 씬의 개수를 가져옵니다.
     private int SceneLength => SceneManager.sceneCountInBuildSettings;
     private void Awake()
     {
@@ -63,6 +69,8 @@ public class GameManager : Singleton<GameManager>
             LoadLastOpenedScene();
         }
     }
+    // 게임 매니저 세팅을 초기화합니다.
+    // 점수를 0점으로 하며, 인덱스 0번의 씬으로 이동합니다.
     public void Init()
     {
         score = 0;
@@ -71,6 +79,7 @@ public class GameManager : Singleton<GameManager>
         currentSceneIndex = 0;
         SceneManager.LoadSceneAsync(currentSceneIndex);
     }
+    // 지정한 인덱스의 씬으로 이동합니다.
     public void LoadScene(int index)
     {
         if (index < 0 || index >= SceneLength)
@@ -81,12 +90,16 @@ public class GameManager : Singleton<GameManager>
         currentSceneIndex = index;
         SceneManager.LoadSceneAsync(currentSceneIndex);
     }
+    // 지정한 이름의 씬으로 이동합니다.
     public void LoadScene(string name)
     {
         lastOpenedSceneIndex = currentSceneIndex;
         currentSceneIndex = SceneManager.GetSceneByName(name).buildIndex;
         SceneManager.LoadSceneAsync(currentSceneIndex);
     }
+    // 가장 마지막에 열렸던 씬으로 이동합니다.
+    // 현재 3번 씬이고, 이전에 1번 씬이었다면, 1번 씬으로 이동하고
+    // 가장 마지막여 열렸던 씬을 3번 씬으로 합니다.
     public void LoadLastOpenedScene()
     {
         if (lastOpenedSceneIndex == -1)
@@ -99,6 +112,8 @@ public class GameManager : Singleton<GameManager>
         SceneManager.LoadSceneAsync(currentSceneIndex);
 
     }
+    // 현재 바로 다음 인덱스의 씬으로 이동합니다.
+    // 예를 들어, 1번 씬에서 2번 씬으로 이동합니다.
     public void LoadNextScene()
     {
         if (currentSceneIndex == SceneLength - 1)
@@ -109,6 +124,8 @@ public class GameManager : Singleton<GameManager>
         currentSceneIndex++;
         SceneManager.LoadSceneAsync(currentSceneIndex);
     }
+    // 현재 바로 다음 인덱스의 씬으로 이동합니다.
+    // 예를 들어, 2번 씬에서 1번 씬으로 이동합니다.
     public void LoadPreviousScene()
     {
         if (currentSceneIndex == 0)
@@ -120,8 +137,14 @@ public class GameManager : Singleton<GameManager>
         SceneManager.LoadSceneAsync(currentSceneIndex);
     }
 
+    // 지정한 정수만큼 점수를 올립니다.
+    // 점수는 maxScore보다 커질 수 없습니다.
     public void ScoreUp(int amount = 1)
     {
+        if (amount <= 0)
+        {
+            amount = 0;
+        }
         int nextScore = score + amount;
         if (nextScore >= maxScore)
         {
@@ -130,8 +153,15 @@ public class GameManager : Singleton<GameManager>
         }
         score = nextScore;
     }
+
+    // 지정한 정수만큼 점수를 내립니다.
+    // 점수는 0 보다 작아질 수 없습니다.
     public void ScoreDown(int amount = 1)
     {
+        if (amount <= 0)
+        {
+            amount = 0;
+        }
         int nextScore = score - amount;
         if (nextScore <= 0)
         {
@@ -141,6 +171,7 @@ public class GameManager : Singleton<GameManager>
         score = nextScore;
     }
 
+    // 게임 오버 시 지정된 이벤트를 실행합니다.
     public void GameOver()
     {
         gameOverEvent.Invoke();
--- Assets/KMS/Singleton.cs
+++ Assets/KMS/Singleton.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 컴포넌트 클래스를 싱글톤으로 만드는 추상 클래스입니다.
+// 컴포넌트 클래스 선언은
+// public class CompoClass : Singleton<CompoClass> { }
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+
+    // CompoClass.Instance 를 통해 해당 컴포넌트의 인스턴스로 접근 가능하도록 하는 프로퍼티입니다.
+    // 만약 인스턴스가 없는데 해당 프로퍼티를 사용한다면
+    // 즉 예를 들어 모르고 싱글톤을 게임에 할당하지 않은 경우
+    // 컴포넌트의 이름으로 새로운 게임오브젝트를 만든 후,
+    // 해당 게임오브젝트에 컴포넌트를 붙입니다.
     public static T Instance
     {
         get
@@ -19,6 +28,9 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         }
     }
 
+    // 컴포넌트는 Awake() 단계에서 아래의 함수를 실행하도록 합니다.
+    // 최초 instance에 인스턴스 할당 시 게임오브젝트가 삭제되지 않도록 고정되며
+    // 이후 추가된 게임오브젝트에 해당 컴포넌트가 붙어있다면, 삭제시킵니다.
     protected void SetInstance()
     {
         if (instance == null)
