51890185cdf15eca90e4f4243703b9e87873c899
neosmu <hojunglee00@naver.com>
2025-04-28 11:04:52 +0900

    [Feat] AttackItem 초기 구조 구현
    추상클래스를 통해 Item 스크립트를 활용하여 플레이어가  공격력 상승을 목표로 AttackItem의 초기 구조 구현

---
+++ Assets/LHJ/Scripts/AttackItem.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackItem : Item
+{
+    public override void RunItem()
+    {
+
+    }
+}
