83736fd6283ce03dcbcefe0061f30471619d9930
neosmu <hojunglee00@naver.com>
2025-04-28 16:26:39 +0900

    [Feat] AttackItem 기능 구현
    matarials을 사용하여 색상을 입히고    프리펩화 시켜 자주 사용할 수 있도록 구성하였다.
    [SerializeField] 를 통해 공격력 증가량과 버프 지속 시간을 조정할 수 있다. 현재는 임의로 공격력 3 증가 버프시간 5초로 설정되어있다.

--- Assets/LHJ/Scripts/AttackItem.cs
+++ Assets/LHJ/Scripts/AttackItem.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 
 public class AttackItem : Item
 {
+    [SerializeField] private int attackIncrease;
+    [SerializeField] private float buffTime;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RunItem();
+        }
+    }
     public override void RunItem()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Testplayercontroll playerController = player.GetComponent<Testplayercontroll>();
+        if (playerController != null)
+        {
+            playerController.StartCoroutine(TemporaryAttackBuff(playerController));
+        }
 
+        Destroy(gameObject); 
+    }
+    private IEnumerator TemporaryAttackBuff(Testplayercontroll player)
+    {
+        player.IncreaseAttack(attackIncrease);
+        yield return new WaitForSeconds(buffTime);
+        player.IncreaseAttack(-attackIncrease);
     }
 }
