136806b6f4b9f95024cc71ef814a0f09798a3c8d
neosmu <hojunglee00@naver.com>
2025-04-28 17:13:39 +0900

    [Feat] SpeedItem 구현
    AttackItem과 마찬가지로 [SerializeField]를 사용하여 플레이어 속도 증가와 버프지속시간을 설정할 수 있으며 현재 임시로 설정 해놓은 값은 속도 증가 : 2, 버프 시간은 동일하게 5초로 설정되어있다.

--- Assets/LHJ/Scripts/SpeedItem.cs
+++ Assets/LHJ/Scripts/SpeedItem.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedItem : MonoBehaviour
+public class SpeedItem : Item
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float speedIncrease;
+    [SerializeField] private float buffTime;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            RunItem();
+        }
+    }
+    public override void RunItem()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Testplayercontroll playerController = player.GetComponent<Testplayercontroll>();
+        if (playerController != null)
+        {
+            playerController.StartCoroutine(TemporaryAttackBuff(playerController));
+        }
+        Destroy(gameObject);
+    }
+    private IEnumerator TemporaryAttackBuff(Testplayercontroll player)
+    {
+        player.IncreaseSpeed(speedIncrease);
+        yield return new WaitForSeconds(buffTime);
+        player.IncreaseSpeed(-speedIncrease);
     }
 }
