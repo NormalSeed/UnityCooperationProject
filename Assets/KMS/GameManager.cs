405ee5d557694d359de82dc31e5a55d117e9c397
munsung143 <akunta3475@gmail.com>
2025-04-28 11:54:41 +0900

    [Feat] 씬 이동 기능 구현
    이름, 인덱스에 따른 씬 이동
    마지막으로 열렸던 씬으로 이동
    이전, 다음 씬으로 이동 기능 구현

--- Assets/KMS/GameManager.cs
+++ Assets/KMS/GameManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,17 +9,51 @@ public class GameManager : Singleton<GameManager>
 {
     [SerializeField] bool isGameOver;
     [SerializeField] int score;
+    private Scene lastOpenedScene;
+    private Scene currentScene;
     private void Awake()
     {
         SetInstance();
     }
-
-    public void ChangeScene(int index)
+    private void Start()
     {
-        SceneManager.LoadScene(index);
+        //previousScene = SceneManager.GetActiveScene();
+        currentScene = SceneManager.GetActiveScene();
     }
-    public void ChangeScene(string name)
+    private void Update()
     {
-        SceneManager.LoadScene(name);
+
+    }
+
+    public void LoadScene(int index)
+    {
+        lastOpenedScene = currentScene;
+        currentScene = SceneManager.GetSceneByBuildIndex(index);
+        SceneManager.LoadSceneAsync(currentScene.buildIndex);
+    }
+    public void LoadScene(string name)
+    {
+        lastOpenedScene = currentScene;
+        currentScene = SceneManager.GetSceneByName(name);
+        SceneManager.LoadSceneAsync(currentScene.buildIndex);
+    }
+    public void LoadLastOpenedScene()
+    {
+        Scene temp = currentScene;
+        currentScene = lastOpenedScene;
+        lastOpenedScene = temp;
+        SceneManager.LoadSceneAsync(currentScene.buildIndex);
+    }
+    public void LoadNextScene()
+    {
+        lastOpenedScene = currentScene;
+        currentScene = SceneManager.GetSceneByBuildIndex(currentScene.buildIndex + 1);
+        SceneManager.LoadSceneAsync(currentScene.buildIndex);
+    }
+    public void LoadPreviousScene()
+    {
+        lastOpenedScene = currentScene;
+        currentScene = SceneManager.GetSceneByBuildIndex(currentScene.buildIndex - 1);
+        SceneManager.LoadSceneAsync(currentScene.buildIndex);
     }
 }
