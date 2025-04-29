1b89f2abdc9fa89d558775c81f150ff303ff48e8
baechu8716 <baechu8716@naver.com>
2025-04-29 19:18:53 +0900

    [Fix] 트랩 오류 해결
    트랩 트리거진입시 작동안하는 오류 해결
    hp가 0이되고 오브젝트풀로 반환되지 않는 오류 해결
    오브젝트풀로 반환된후 다시 활성화 될때 데미지 입는 로직을 해결

--- Assets/YTW/Scripts/AttackTrap.cs
+++ Assets/YTW/Scripts/AttackTrap.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,66 +6,96 @@ public class AttackTrap : MonoBehaviour
 {
     [SerializeField] int damage;
     [SerializeField] float attackCoolTime = 1.0f;
-    [SerializeField] float speedDebuffPercent = 0.3f; 
+    [SerializeField] float speedDebuffPercent = 0.3f;
 
-    private bool attackRange = false;
-    private float lastAttackTime = 0f;
-    private MonsterController targetMonster;
-    private float originalSpeed;
+    private List<MonsterController> monstersInTrap = new List<MonsterController>();
+    private Dictionary<MonsterController, float> lastAttackTimes = new Dictionary<MonsterController, float>();
+    private Dictionary<MonsterController, float> originalSpeeds = new Dictionary<MonsterController, float>();
 
     private void Update()
     {
-       if (attackRange && targetMonster != null)
-        {
-            if(Time.time >= attackCoolTime + lastAttackTime)
-            {
-                Attack(targetMonster.transform);
-                lastAttackTime = Time.time;
-            }
-        }
+        // 트랩내 몬스터가 없다고 불필요하게 돌릴 필요 없으니
+        if (monstersInTrap.Count == 0) return;
+        AttackCool();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Monster"))
+
+        MonsterController monster = other.GetComponent<MonsterController>();
+        // 몬스터가 null이 아니고 트랩안몬스터에 몬스터가 없다면
+        if (monster != null && !monstersInTrap.Contains(monster))
         {
-            Debug.Log("트랩 범위에 몬스터가 들어옴");
-            targetMonster = other.GetComponent<MonsterController>();
-            if (targetMonster != null)
+            // 트랩안 몬스터 추가
+            monstersInTrap.Add(monster);
+            Debug.Log("몬스터가 트랩에 들어옴");
+
+            // 속도감소를 위해 NavMeshAgent컴포넌트 가져옴
+            NavMeshAgent agent = monster.GetComponent<NavMeshAgent>();
+            if (agent != null)
             {
-                attackRange = true;
-                NavMeshAgent agent = targetMonster.GetComponent<NavMeshAgent>();
-                if (agent != null)
+                if (!originalSpeeds.ContainsKey(monster))
                 {
-                    originalSpeed = agent.speed;
-                    agent.speed = originalSpeed * (1f - speedDebuffPercent); 
+                    originalSpeeds[monster] = agent.speed;
                 }
+                agent.speed = originalSpeeds[monster] * (1f - speedDebuffPercent);
             }
+
         }
+
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Monster"))
+        MonsterController monster = other.GetComponent<MonsterController>();
         {
-            Debug.Log("트랩 범위에 몬스터가 나감");
-            if (targetMonster != null)
+            if(monster != null && monstersInTrap.Contains(monster))
             {
-                NavMeshAgent agent = targetMonster.GetComponent<NavMeshAgent>();
-                if (agent != null)
+                monstersInTrap.Remove(monster);
+                lastAttackTimes.Remove(monster);
+
+                NavMeshAgent agent = monster.GetComponent<NavMeshAgent>();
+                if (agent != null && originalSpeeds.ContainsKey(monster))
                 {
-                    agent.speed = originalSpeed;
+                    agent.speed = originalSpeeds[monster];
                 }
             }
-            attackRange = false;
-            targetMonster = null;
+            Debug.Log("몬스터가 트랩에서 나감");
         }
     }
 
-    public void Attack(Transform target)
+    public void Attack(MonsterController monster)
     {
-        OnDamaged targetHP = target.GetComponent<OnDamaged>();
+        OnDamaged targetHP = monster.GetComponent<OnDamaged>();
         targetHP?.TakeDamaged(damage);
         Debug.Log($"몬스터 공격 남은 HP : {targetHP.CURHP}");
+    }
+
+    public void AttackCool()
+    {
+        for (int i = monstersInTrap.Count - 1; i >= 0; i--)
+        {
+            MonsterController monster = monstersInTrap[i];
+            if (monster == null || !monster.gameObject.activeInHierarchy)
+            {
+                monstersInTrap.RemoveAt(i);
+                lastAttackTimes.Remove(monster);
+                originalSpeeds.Remove(monster);
+                Debug.Log("리스트내 죽은 몬스터 제거");
+                continue;
+            }
+
+            if (!lastAttackTimes.ContainsKey(monster))
+            {
+                lastAttackTimes[monster] = 0;
+            }
+
+            if (Time.time - lastAttackTimes[monster] >= attackCoolTime)
+            {
+                Attack(monster);
+                lastAttackTimes[monster] = Time.time;
+            }
+        }
     }
 
 }
