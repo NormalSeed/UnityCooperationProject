bc91e9d19a011f818bdd7fa2af9909261d2bcf82
baechu8716 <baechu8716@naver.com>
2025-04-30 18:39:49 +0900

    [Feat] 원거리 몬스터 추가
    플레이어를 바라보며 총을 쏘는 원거리 몬스터 추가

--- Assets/YTW/Scripts/Bullet.cs
+++ Assets/YTW/Scripts/Bullet.cs
@@ -7,13 +7,22 @@ public class Bullet : MonoBehaviour
     [SerializeField] PooledBullet pooledBullet;
     [SerializeField] int damage;
 
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        OnDamaged targetHP = other.GetComponent<OnDamaged>();
-        if (targetHP != null)
+        GameObject hitObject = collision.gameObject;
+
+        OnDamaged targetHP = hitObject.GetComponent<OnDamaged>();
+        if (targetHP != null && targetHP.gameObject.CompareTag("Player"))
         {
             targetHP.TakeDamaged(damage);
             pooledBullet.ReturnPool();
         }
+        if (hitObject.layer == LayerMask.NameToLayer("Wall") || hitObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            // 터지는 이팩트 
+            pooledBullet.ReturnPool();
+        }
+        
     }
 }
