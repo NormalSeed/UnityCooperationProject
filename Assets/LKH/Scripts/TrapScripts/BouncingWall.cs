22887e72257936169467a43da07c52d6297a1a97
NormalSeed <dlrjsghk11@naver.com>
2025-04-30 12:59:30 +0900

    [Fix] 튕기는 벽 로직 수정
    튕기는 벽과 충돌했을 때 일정한 속도로 튕겨져 나가도록 로직 수정

--- Assets/LKH/Scripts/TrapScripts/BouncingWall.cs
+++ Assets/LKH/Scripts/TrapScripts/BouncingWall.cs
@@ -7,13 +7,14 @@ public class BouncingWall : MonoBehaviour
     [SerializeField] float bounceSpd;
     private void OnCollisionEnter(Collision collision)
     {
+        Vector3 incomingVec = collision.relativeVelocity.normalized;
+        Vector3 colliderNormalVec = collision.contacts[0].normal;
+        Vector3 reflectVec = Vector3.Reflect(incomingVec, colliderNormalVec);
+        Rigidbody colliderRb = collision.gameObject.GetComponent<Rigidbody>();
+
         if (collision.gameObject.CompareTag("Player"))
         {
-            Vector3 incomingVec = collision.relativeVelocity.normalized;
-            Vector3 colliderNormalVec = collision.contacts[0].normal;
-            Vector3 reflectVec = Vector3.Reflect(incomingVec, colliderNormalVec);
-            Rigidbody colliderRb = collision.gameObject.GetComponent<Rigidbody>();
-            colliderRb.AddForce(reflectVec * bounceSpd, ForceMode.Impulse);
+            colliderRb.velocity = reflectVec * bounceSpd;
         }
     }
 }
