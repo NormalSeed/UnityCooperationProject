20427eecb97292c789e39f3cc7bf51e2dad0b780
kimdongjun22 <dkdl01028@icloud.com>
2025-04-30 17:15:07 +0900

    [FEAT]플레이어 체력및착지등 추가기능
    공이 몬스터에 닿으면 체력 감소, 0이면 Game Over
    추가적으로 플레이어 공이 가는 방향으로 회전하고 기존 점프 상하좌우에서 착지기능 구현하고 나중에 회피구현도 넣을예정

--- Assets/KDJ/script/Ball.cs
+++ Assets/KDJ/script/Ball.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float ballSpeed;
-    public Vector3 ballMovementVector;
-    Rigidbody rb;
+    //Ball이 어떤 벡터 방향으로 움직이다가 벽 또는 플레이어에 닿았을때 방향전환 
+    public float ballSpeed; // 공이 움직이는 속도 (인스펙터에서 설정)
+    public Vector3 ballMovementVector;// 공이 이동할 방향 벡터
+    Rigidbody rb;// Rigidbody 컴포넌트 참조
 
     private void Awake()
     {
@@ -16,15 +17,18 @@ public class Ball : MonoBehaviour
     {
         
     }
+    //게임오브젝트.transform.up이 월드 기준으로 평면에서 수직으로 올라가는 벡터
     void Update()
     {
         transform.Translate(ballMovementVector.normalized * ballSpeed * Time.deltaTime);
     }
     private void OnCollisionEnter(Collision collision)
     {
+        //충돌한 오브젝트가 wall 또는 Player인 경우 방향을 반사
         GameObject go = collision.gameObject;
-        if (go.CompareTag("wall") || go.CompareTag("Player"))
+        if (go.CompareTag("wall") || go.CompareTag("Player"))//부딫힌 면에 대한 방향 전환
         {
+            //충돌한 면의 위 방향 벡터를 기준으로 어느 방향에 닿았는지 판단
             if (go.transform.up.x < -0.01f || go.transform.up.x > 0.01f)
                 ballMovementVector.x *= -1;
             else if (go.transform.up.y < -0.01f || go.transform.up.y > 0.01f)
