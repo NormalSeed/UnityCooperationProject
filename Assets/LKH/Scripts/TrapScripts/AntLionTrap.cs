c74750724e4ebefd7b40b0c5242a8e85f5cf8baf
NormalSeed <dlrjsghk11@naver.com>
2025-05-02 12:21:27 +0900

    [Fix] 개미지옥 트랩 로직 수정
    개미지옥 트랩에서 탈출로직 구현

--- Assets/LKH/Scripts/TrapScripts/AntLionTrap.cs
+++ Assets/LKH/Scripts/TrapScripts/AntLionTrap.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class AntLionTrap : MonoBehaviour
 {
-    [SerializeField] Transform trapCenter;
-    [SerializeField] float pullPower;
+    [SerializeField] float maxPullForce;    // 최대 끌어당기는 힘
+    public float escapeForce;               // 빠져나갈 때 가해주는 힘
+    public float minDistance;               // 최소 거리 제한
+    public float escapeThreshold;           // 일정 속도 이상일 때 탈출 가능
+    public Transform trapCenter;            // 트랩 중심
 
-    private void OnTriggerStay(Collider other)
+    void OnTriggerStay(Collider other)
     {
-        if (other.attachedRigidbody != null) // 닿은 물체에 리지드바디가 있다면
+        if (other.attachedRigidbody != null)
         {
-            Vector3 direction = (trapCenter.position - other.transform.position).normalized; // 방향을 trapCenter 방향으로
-            other.attachedRigidbody.AddForce(direction * pullPower, ForceMode.Impulse);
+            Rigidbody rb = other.attachedRigidbody;
+            float distance = Vector3.Distance(trapCenter.position, other.transform.position);
+
+            if (distance > minDistance)
+            {
+                float forceMagnitude = maxPullForce / distance; // 거리에 반비례하는 힘 설정
+                Vector3 direction = (trapCenter.position - other.transform.position).normalized;
+                rb.AddForce(direction * forceMagnitude, ForceMode.Acceleration);
+
+                // 플레이어가 빠져나가려 할 때 일정 속도 이상이면 탈출 가능
+                if (rb.velocity.magnitude > escapeThreshold)
+                {
+                    Vector3 escapeDirection = rb.velocity.normalized;
+                    rb.AddForce(escapeDirection * escapeForce, ForceMode.Acceleration);
+                }
+            }
         }
     }
+
 }
