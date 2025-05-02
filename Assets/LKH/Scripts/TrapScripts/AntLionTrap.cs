32541618fb5bd2164ca95584e27d2aded7f3b012
NormalSeed <dlrjsghk11@naver.com>
2025-05-02 12:57:42 +0900

    [Chore] 개미지옥 트랩 스크립트 수정
    개미지옥 트랩 스크립트 설명 주석 제거

--- Assets/LKH/Scripts/TrapScripts/AntLionTrap.cs
+++ Assets/LKH/Scripts/TrapScripts/AntLionTrap.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class AntLionTrap : MonoBehaviour
 {
-    [SerializeField] float maxPullForce;    // 최대 끌어당기는 힘
-    public float escapeForce;               // 빠져나갈 때 가해주는 힘
-    public float minDistance;               // 최소 거리 제한
-    public float escapeThreshold;           // 일정 속도 이상일 때 탈출 가능
-    public Transform trapCenter;            // 트랩 중심
+    [SerializeField] float maxPullForce;
+    public float escapeForce;           
+    public float minDistance;           
+    public float escapeThreshold;       
+    public Transform trapCenter;        
 
     void OnTriggerStay(Collider other)
     {
@@ -19,11 +19,10 @@ public class AntLionTrap : MonoBehaviour
 
             if (distance > minDistance)
             {
-                float forceMagnitude = maxPullForce / distance; // 거리에 반비례하는 힘 설정
+                float forceMagnitude = maxPullForce / distance;
                 Vector3 direction = (trapCenter.position - other.transform.position).normalized;
                 rb.AddForce(direction * forceMagnitude, ForceMode.Acceleration);
 
-                // 플레이어가 빠져나가려 할 때 일정 속도 이상이면 탈출 가능
                 if (rb.velocity.magnitude > escapeThreshold)
                 {
                     Vector3 escapeDirection = rb.velocity.normalized;
